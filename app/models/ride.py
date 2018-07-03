4b691021af43400e6fd35586a51c9d8d569ef1bd
SelaDanti <kwameasiago@gmail.com>
2018-07-03 07:09:43 +0300

    [feature #158665368] Add verification for ride slots

--- app/models/ride.py
+++ app/models/ride.py
@@ -1,10 +1,13 @@
+import re
+import datetime
+
 class CheckRideData:
     def __init__(self):
         self.DataErr = None
 
 
     def CheckEmptyData(self, items):
-        items = list(items.values())
+        items = [items['start'], items['finish'], items['email'], items['date']]
         for item in items:
             if not item:
                 self.dataError = {'result':'Invalid input (Empty data)'},405
@@ -13,7 +16,7 @@ class CheckRideData:
 
 
     def CheckwhiteSpaceData(self, items):
-        items = list(items.values())
+        items = [items['start'], items['finish'], items['email'], items['date']]
         for item in items:
             if item.isspace():
                 self.dataError = {'result':'Invalid input(Only whitespace)'},405
@@ -33,18 +36,18 @@ class CheckRideData:
 
 
     def CheckLocation(self, items):
-        start, finish = items['start'], item['finish']
+        start, finish = items['start'], items['finish']
         if start == finish:
             self.dataError = {'result': 'Start point and Finish point are the same'}
         else:
             return False
 
 
-    def checkDate(self, items):
-        if '-' not in items['dob']:
+    def CheckDate(self, items):
+        if '-' not in items['date']:
             self.dataError = {'result':'Invalid Date'},405
             return(self.dataError)
-        date = items['dob'].split('-')
+        date = items['date'].split('-')
         date = list(map(int, date))
         day,month,year = int(date[0]), int(date[1]), int(date[2])
         ValidDate = True
@@ -60,6 +63,22 @@ class CheckRideData:
             return(False)
 
 
+    def CheckSlots(self,items):
+        if items['slot'] < 0:
+            self.dataError = {'result':'Invalid slot number(less than zero)'},405
+            return(self.dataError)
+        else:
+            try:
+                items['slot'].is_integer()
+                self.dataError = {'result':'Invalid slot number(Mast be an int)'},405
+                return(self.dataError)
+            except AttributeError:
+                return(False)
+
+            
+
+
+
 class ModifyRide(CheckRideData):
     def __init__(self,data):
         self.data = data
@@ -67,4 +86,21 @@ class ModifyRide(CheckRideData):
         self.CheckwhiteSpaceData = CheckRideData.CheckwhiteSpaceData(self, self.data)
         self.CheckEmail = CheckRideData.CheckEmail(self, self.data)
         self.CheckLocation = CheckRideData.CheckLocation(self, self.data)
-        self.checkDate = CheckRideData.checkDate(self, self.data)
+        self.CheckDate = CheckRideData.CheckDate(self, self.data)
+        self.CheckSlots = CheckRideData.CheckSlots(self,self.data)
+
+    def UploadRide(self):
+        if self.CheckEmptyData is not False:
+            return(self.CheckEmptyData)
+        elif self.CheckwhiteSpaceData is not False:
+            return(self.CheckwhiteSpaceData)
+        elif self.CheckEmail is not False:
+            return(self.CheckEmail)
+        elif self.CheckLocation is not False:
+            return(self.CheckLocation)
+        elif self.CheckDate is not False:
+            return(self.CheckDate)
+        elif self.CheckSlots is not False:
+            return(self.CheckSlots)
+        else:
+            pass
